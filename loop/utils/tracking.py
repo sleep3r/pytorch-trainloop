8071720be734379e4b3f5070a70c80dc178b172e
Alexander Kalashnikov <33665156+sleep3r@users.noreply.github.com>
2022-04-16 05:34:06 +0300

    Update tracking.py

--- loop/utils/tracking.py
+++ loop/utils/tracking.py
@@ -1,7 +1,10 @@
+import json
 from typing import Union
 
 import wandb
 from pathlib import Path
+
+from utils.path import mkdir_or_exist
 
 
 def log_artifact(name: str, artifact_path: Union[str, Path], type: str) -> None:
@@ -11,3 +14,20 @@ def log_artifact(name: str, artifact_path: Union[str, Path], type: str) -> None:
     artifact = wandb.Artifact(name, type=type)
     artifact.add_file(artifact_path)
     wandb.log_artifact(artifact)
+
+
+def save_splits(train_subsamples, val_subsamples, cv_step, exp_dir: Path):
+    mkdir_or_exist(str(exp_dir / "CV_splits"))
+
+    with open(exp_dir / "CV_splits" / f"{cv_step + 1}.json", "w") as f:
+        splits = {
+            "train": {
+                "pneumonia": train_subsamples[0].tolist(),
+                "norma": train_subsamples[1].tolist()
+            },
+            "val": {
+                "pneumonia": val_subsamples[0].tolist(),
+                "norma": val_subsamples[1].tolist()
+            }
+        }
+        json.dump(splits, f, indent=2)
